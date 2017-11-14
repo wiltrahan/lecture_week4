1d5fe3078c9f1ce50e8704e035826d1cd001c1b1
Wil Trahan <wiltrahan@intracitygeeks.org>
2017-11-14 22:57:21 +0000

    week4 lecture

---
+++ memory.c
@@ -0,0 +1,14 @@
+//look up valgrind docs
+
+#include <stdlib.h>
+
+void f(void) {
+    int *x = malloc(10 * sizeof(int));
+    x[9] = 0;
+    free(x);
+}
+
+int main(void) {
+    f();
+    return 0;
+}
---
+++ structs.h
@@ -0,0 +1,7 @@
+#include <cs50.h>
+
+typedef struct {
+    string name;
+    string dorm;
+}
+student;
